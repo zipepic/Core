2dcd98c73d9f69d4bb6995a28f037ff09108897d
zipepic <olezhka.kirilov.97@mail.ru>
2023-09-27 16:04:17 +0300

    Created ApplicationRegisteredEvent

--- src/main/java/com/project/core/events/ApplicationRegisteredEvent.java
+++ src/main/java/com/project/core/events/ApplicationRegisteredEvent.java
@@ -1,5 +1,10 @@
 package com.project.core.events;
 
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
 public class ApplicationRegisteredEvent {
   private String clientId;
   private String code;
