02e4e0ef2597644333846f41e61340dff021defa
zipepic <olezhka.kirilov.97@mail.ru>
2023-10-04 23:29:09 +0300

    Added work with the method from JwtTokenUtils to create a list of tokens

--- src/main/java/com/project/core/commands/token/GenerateTokenCommand.java
+++ src/main/java/com/project/core/commands/token/GenerateTokenCommand.java
@@ -1,8 +1,12 @@
 package com.project.core.commands.token;
 
+import com.project.core.commands.ResourceServerDTO;
 import lombok.Builder;
 import lombok.Data;
 import org.axonframework.modelling.command.TargetAggregateIdentifier;
+
+import java.util.List;
+
 @Data
 @Builder
 public class GenerateTokenCommand {
@@ -11,4 +15,6 @@ public class GenerateTokenCommand {
   private String userId;
   private String clientId;
   private String scope;
+
+  private List<ResourceServerDTO> resourceServerDTOList;
 }
