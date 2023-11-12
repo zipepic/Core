a46efe436c9e10d071e4cba14acc5e02b9f027cf
zipepic <olezhka.kirilov.97@mail.ru>
2023-11-12 19:32:39 +0300

    Added parseRSAKeyFromSimpleJWK

--- src/main/java/tokenlib/util/jwk/RSAParser.java
+++ src/main/java/tokenlib/util/jwk/RSAParser.java
@@ -2,7 +2,6 @@ package tokenlib.util.jwk;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.nimbusds.jose.jwk.JWK;
 import com.nimbusds.jose.jwk.RSAKey;
 import com.nimbusds.jose.util.Base64URL;
 
@@ -10,7 +9,7 @@ public class RSAParser {
   public static RSAKey parseRSAKeyFromJson(String json) throws JsonProcessingException {
 
       ObjectMapper objectMapper = new ObjectMapper();
-      var jwk = objectMapper.readValue(json, Jwk.class);
+      var jwk = objectMapper.readValue(json, SimpleJWK.class);
 
       RSAKey rsaKey = new RSAKey.Builder(
         new Base64URL(jwk.getN()),
@@ -18,6 +17,15 @@ public class RSAParser {
         .keyID(jwk.getKid())
         .build();
       return rsaKey;
+
+  }
+  public static RSAKey parseRSAKeyFromSimpleJWK(SimpleJWK jwk) throws JsonProcessingException {
+    RSAKey rsaKey = new RSAKey.Builder(
+      new Base64URL(jwk.getN()),
+      new Base64URL(jwk.getE()))
+      .keyID(jwk.getKid())
+      .build();
+    return rsaKey;
 
   }
 }
