42b9062d8c2c3cf91b558551eec2de1c2df4b0b1
zipepic <olezhka.kirilov.97@mail.ru>
2023-11-21 23:48:45 +0300

    Added new query UserProfileLookupQuery

---
+++ src/main/java/com/project/core/queries/user/UserProfileLookupQuery.java
@@ -0,0 +1,11 @@
+package com.project.core.queries.user;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public final class UserProfileLookupQuery {
+  private String userId;
+  private String userName;
+}
