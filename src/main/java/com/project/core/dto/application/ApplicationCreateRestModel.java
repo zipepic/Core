3e48e1a876770cddf225f476bbcb8611d1d92137
xzz1p <olezhka.kirilov.97@mail.ru>
2023-12-11 00:21:13 +0300

    Removed unused methods, commands, aggregates, etc.

--- src/main/java/com/project/core/dto/application/ApplicationCreateRestModel.java
+++
@@ -1,9 +0,0 @@
-package com.project.core.dto.application;
-
-import lombok.Data;
-
-@Data
-public class ApplicationCreateRestModel {
-  private String clientId;
-  private String secret;
-}
--- src/main/java/com/project/core/queries/user/FetchJwkSet.java
+++
@@ -1,9 +0,0 @@
-package com.project.core.queries.user;
-
-import lombok.Builder;
-import lombok.Data;
-
-@Data
-@Builder
-public final class FetchJwkSet {
-}
