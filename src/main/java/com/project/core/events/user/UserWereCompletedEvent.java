68a70a5cd4e428dd68521f13c5152d2c8f9fc70c
xzz1p <olezhka.kirilov.97@mail.ru>
2023-12-10 23:52:14 +0300

    Enhance UserWereCompletedEvent with providerId and authProvider

--- src/main/java/com/project/core/events/user/UserWereCompletedEvent.java
+++ src/main/java/com/project/core/events/user/UserWereCompletedEvent.java
@@ -2,10 +2,13 @@ package com.project.core.events.user;
 
 import lombok.Builder;
 import lombok.Data;
+import tokenlib.util.jwk.AuthProvider;
 
 @Data
 @Builder
 public class UserWereCompletedEvent {
     private String userId;
+    private String providerId;
+    private AuthProvider authProvider;
     private String status;
 }
