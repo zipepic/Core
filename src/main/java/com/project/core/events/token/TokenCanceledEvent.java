bae74fd87eed127b029ee0de74deac28b7ca6ad8
xzz1p <olezhka.kirilov.97@mail.ru>
2023-12-10 23:42:02 +0300

    Added token id

--- src/main/java/com/project/core/events/token/TokenCanceledEvent.java
+++ src/main/java/com/project/core/events/token/TokenCanceledEvent.java
@@ -1,10 +1,12 @@
 package com.project.core.events.token;
 
+import com.project.core.dto.TokenId;
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder
 public class TokenCanceledEvent {
+    private TokenId tokenFromUserId;
     private String userId;
 }
