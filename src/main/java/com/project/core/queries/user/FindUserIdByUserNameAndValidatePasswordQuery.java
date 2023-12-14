b30a5aef50990e208f872d495bbee471368f268d
xzz1p <olezhka.kirilov.97@mail.ru>
2023-12-14 23:05:12 +0300

    Initial commit

---
+++ src/main/java/com/project/core/queries/user/FindUserIdByUserNameAndValidatePasswordQuery.java
@@ -0,0 +1,11 @@
+package com.project.core.queries.user;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class FindUserIdByUserNameAndValidatePasswordQuery {
+    private String userName;
+    private String password;
+}
